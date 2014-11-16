d0a3c0f09bbbabdda04d279a8babd6758005a15f
Sasha Mazurov <alexander.mazurov@gmail.com>
2014-11-16 19:57:35 +0000

    Print classes and classes attributes

---
+++ main.py
@@ -0,0 +1,4 @@
+from win32com.client import gencached
+m = gencache.EnsureModule('{B9C079AA-92DD-4FB4-A0E0-AA3198955B45}', 0, 1, 0)
+
+print("Hello world")
---
+++ scripts/start.bat
@@ -0,0 +1,2 @@
+cd /D z:\mfiles-report
+venv\Scripts\activate.bat & ipython notebook --ip=10.211.55.3
